eeef05837a69d87175afbf08b95b2cf62a744254
Alex Lukyanau <cybergomel@gmail.com>
2014-08-13 05:44:47 -0400

    Configuration file 3

--- skin/frontend/default/sashasv2/config.rb
+++ skin/frontend/default/sashasv2/config.rb
@@ -1,12 +1,10 @@
-http_path = "/skin/frontend/default/sashasv2/"
-css_dir = "../css"
-sass_dir = "../scss"
-images_dir = "../images"
-javascripts_dir = "../js"
+#http_path = "/skin/frontend/default/sashasv2/"
+css_dir = "css"
+sass_dir = "scss"
+#images_dir = "../images"
+#javascripts_dir = "../js"
 relative_assets = true
- 
-
-add_import_path "../../rwd/default/scss/"
+add_import_path "../../rwd/default/scss"
 
 output_style=:expanded
 environment=:production
