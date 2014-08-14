c9d5464cce0caa878cdea302a7e30d58c5a10def
Alex Lukyanau <cybergomel@gmail.com>
2014-08-14 01:43:01 -0400

    Ruby COnfig

--- skin/frontend/default/sashasv2/config.rb
+++ skin/frontend/default/sashasv2/config.rb
@@ -1,8 +1,5 @@
-#http_path = "/skin/frontend/default/sashasv2/"
 css_dir = "css"
 sass_dir = "scss"
-#images_dir = "../images"
-#javascripts_dir = "../js"
 relative_assets = true
 add_import_path "../../rwd/default/scss"
 
