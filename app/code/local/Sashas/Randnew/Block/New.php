70925d39c399fe6f0e6a3361bd1504d3b620e06a
Alex Lukyanau <cybergomel@gmail.com>
2014-08-24 22:24:29 -0400

    Random New Product

---
+++ app/code/local/Sashas/Randnew/Block/New.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author		Sashas
+ * @category    Sashas
+ * @package     Sashas_Randnew
+ * @copyright   Copyright (c) 2014 Sashas IT Support Inc. (http://www.sashas.org)
+ * @license     http://opensource.org/licenses/GPL-3.0  GNU General Public License, version 3 (GPL-3.0)
+ */
+
+class Sashas_Randnew_Block_New extends Mage_Catalog_Block_Product_New
+{
+	protected function _beforeToHtml()
+	{
+		die();
+		parent::_beforeToHtml();
+		$collection=$this->getCollection();
+		$collection->getSelect()->order('rand()');
+		$this->setProductCollection($collection);
+		return $this;
+	}
+}
